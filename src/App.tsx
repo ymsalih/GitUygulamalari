72f334b5409795ecd045fbce5a88b5a3eeca1881
muhammed salih atic <salihatic@gmail.com>
2026-03-05 14:05:23 +0300

    feat: Vite projesi ana dizine taşındı ve düzeltildi.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>merhaba ben salih ilk deneme uygulamam </p>
+    </>
+  )
+}
+
+export default App
